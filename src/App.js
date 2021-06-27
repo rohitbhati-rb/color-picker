172917779be603389b26fe2ee7b6e5d1d2b5f6f3
Rohit Bhati <rohitbhati8833@gmail.com>
2021-06-27 23:37:00 +0530

    uptill 175.  color box was added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
-import './App.css';
-
+import Palette from './Palette';
+import seedColors from './seedColors';
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Palette {...seedColors[5]}/>
     </div>
   );
 }
--- src/Palette.js
+++ src/Palette.js
@@ -1,9 +1,23 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+// Components
+import ColorBox from './ColorBox';
+
+// CSS
+import './css/Palette.css';
 
 class Palette extends Component {
   render() {
+    const colorBoxes = this.props.colors.map(color => (
+      <ColorBox background={color.color} name={color.name} />
+    ));
     return (
-      <div>
+      <div className="Palette">
+        {/* Navbar goes here */}
+        <div className="Palette-colors">
+          {colorBoxes}
+        </div>
+        {/* footer eventually */}
       </div>
     )
   }
