40f9510c712f2ff5cee64d3ddb5ca3d93f918074
Rohit Bhati <rohitbhati8833@gmail.com>
2021-06-29 18:50:29 +0530

    basic styles added for color slider

--- src/Palette.js
+++ src/Palette.js
@@ -5,8 +5,8 @@ import ColorBox from './ColorBox';
 import Slider from 'rc-slider';
 
 // CSS
-import './css/Palette.css';
 import 'rc-slider/assets/index.css';
+import './css/Palette.css';
 
 class Palette extends Component {
   constructor(props) {
@@ -25,13 +25,15 @@ class Palette extends Component {
     ));
     return (
       <div className="Palette">
-        <Slider
-          defaultValue={this.state.level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={this.changeLevel}
-        />
+        <div className="slider">
+          <Slider
+            defaultValue={this.state.level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={this.changeLevel}
+          />
+        </div>
         {/* Navbar goes here */}
         <div className="Palette-colors">
           {colorBoxes}
