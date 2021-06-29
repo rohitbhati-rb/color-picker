e944da1da9a297079793075c97610f79de45eb62
Rohit Bhati <rohitbhati8833@gmail.com>
2021-06-29 22:24:19 +0530

    Select Menu added from MaterialUI

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,14 +1,26 @@
 import React, { Component } from 'react';
 
 // Components
+import Select from '@material-ui/core/Select';
+import { MenuItem } from '@material-ui/core';
 import Slider from 'rc-slider';
 // CSS
 import 'rc-slider/assets/index.css';
 import './css/Navbar.css';
 
 export class Navbar extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { format: "hex" };
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.setState({ format: e.target.value });
+    this.props.handleChange(e.target.value);
+  }
   render() {
     const { level, changeLevel } = this.props;
+    const { format } = this.state;
     return (
       <header className="Navbar">
         <div className="logo">
@@ -24,7 +36,15 @@ export class Navbar extends Component {
               step={100}
               onAfterChange={changeLevel}
             />
-          </div></div>
+          </div>
+        </div>
+        <div className="select-container">
+          <Select value={format} onChange={this.handleChange}>
+            <MenuItem value="hex">HEX - #ffffff</MenuItem>
+            <MenuItem value="rgb">RGB - rgb(255,255,255)</MenuItem>
+            <MenuItem value="rgba">RGBA - rgba(255,255,255,1.0)</MenuItem>
+          </Select>
+        </div>
       </header>
     )
   }
