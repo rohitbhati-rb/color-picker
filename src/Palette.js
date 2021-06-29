5259e5dac5ed1f041cf6c31c2ca1ac30dcbf9d7c
Rohit Bhati <rohitbhati8833@gmail.com>
2021-06-29 23:14:40 +0530

    Palette footer was added

--- src/Palette.js
+++ src/Palette.js
@@ -21,10 +21,10 @@ class Palette extends Component {
     this.setState({ format: val });
   }
   render() {
-    const { colors } = this.props.palette;
+    const { colors, paletteName, emoji } = this.props.palette;
     const { level, format } = this.state;
     const colorBoxes = colors[level].map(color => (
-      <ColorBox background={color[format]} name={color.name} />
+      <ColorBox key={color.id} background={color[format]} name={color.name} />
     ));
     return (
       <div className="Palette">
@@ -33,11 +33,13 @@ class Palette extends Component {
           changeLevel={this.changeLevel}
           handleChange={this.changeFormat}
         />
-        {/* Navbar goes here */}
         <div className="Palette-colors">
           {colorBoxes}
         </div>
-        {/* footer eventually */}
+        <footer className="Palette-footer">
+          {paletteName}
+          <span className="emoji">{emoji}</span>
+        </footer>
       </div>
     )
   }
