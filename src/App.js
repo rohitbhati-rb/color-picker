03d68180642402afee250cdfe0789b77d979e41f
Rohit Bhati <rohitbhati8833@gmail.com>
2021-06-30 23:05:19 +0530

    Router Links were added for PaletteList and  Links

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Palette from './Palette';
+import PaletteList from './PaletteList';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
@@ -17,7 +18,7 @@ class App extends Component {
         <Route
           exact
           path="/"
-          render={() => <h1>Palette List goes here</h1>}
+          render={() => <PaletteList palettes={seedColors} />}
         />
         <Route
           exact
--- src/Navbar.js
+++ src/Navbar.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 // Components
 import { Select, MenuItem, Snackbar, IconButton } from '@material-ui/core';
@@ -28,7 +29,7 @@ export class Navbar extends Component {
     return (
       <header className="Navbar">
         <div className="logo">
-          <a href="#">reactColorPicker</a>
+          <Link to='/'>reactColorPicker</Link>
         </div>
         <div className="slider-container">
           <span>Level: {level}</span>
