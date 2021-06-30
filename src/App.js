5b766d0b3e7a0400707df1271df2df595dbb591f
Rohit Bhati <rohitbhati8833@gmail.com>
2021-06-30 22:47:14 +0530

    Pallete Link added from router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,37 @@
+import React, { Component } from 'react';
 import { Route, Switch } from 'react-router-dom';
 
 import Palette from './Palette';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
-function App() {
-  return (
-    <Switch>
-      <Route exact path="/" render={() => <h1>Palette List goes here</h1>} />
-      <Route exact path="/palette/:id" render={() => <h1>Individual Palette</h1>} />
-    </Switch>
-    // <div>
-    //   <Palette palette={generatePalette(seedColors[2])} />
-    // </div>
-  );
+class App extends Component {
+  findPalette(id) {
+    return seedColors.find(function (palette) {
+      return palette.id === id;
+    });
+  }
+  render() {
+    return (
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={() => <h1>Palette List goes here</h1>}
+        />
+        <Route
+          exact
+          path="/palette/:id"
+          render={(routeProps) =>
+            <Palette palette={generatePalette(this.findPalette(routeProps.match.params.id))} />
+          }
+        />
+      </Switch>
+      // <div>
+      //   <Palette palette={generatePalette(seedColors[2])} />
+      // </div>
+    );
+  };
 }
 
 export default App;
