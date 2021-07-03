d6874637e87e975f520045bea708f69951f778c6
Rohit Bhati <rohitbhati8833@gmail.com>
2021-07-03 16:12:51 +0530

    added file for creating new  form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { Route, Switch } from 'react-router-dom';
 import Palette from './Palette';
 import PaletteList from './PaletteList';
 import SingleColorPalette from './SingleColorPalette';
+import NewPaletteForm from './NewPaletteForm';
 import seedColors from './seedColors';
 import { generatePalette } from './colorHelpers';
 
@@ -17,6 +18,11 @@ class App extends Component {
   render() {
     return (
       <Switch>
+        <Route
+          exact
+          path="/palette/new"
+          render={() => <NewPaletteForm />}
+        />
         <Route
           exact
           path="/"
--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { withStyles } from '@material-ui/styles';
 
 // Components
@@ -18,6 +19,7 @@ export class PaletteList extends Component {
         <div className={classes.container}>
           <nav className={classes.nav}>
             <h1>React Colors</h1>
+            <Link to="/palette/new">Create Palette</Link>
           </nav>
           <div className={classes.palettes}>
             {palettes.map(palette => (
