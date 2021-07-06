254cd4f9a929a396dfe9f0e5ee41d74139ba68e3
Rohit Bhati <rohitbhati8833@gmail.com>
2021-07-06 20:17:16 +0530

    local storage added for saving palettes on browser

--- src/App.js
+++ src/App.js
@@ -12,7 +12,8 @@ import { generatePalette } from './colorHelpers';
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = ({ palettes: seedColors });
+    const savedPalettes = JSON.parse(window.localStorage.getItem("palettes"));
+    this.state = ({ palettes: savedPalettes || seedColors });
     this.findPalette = this.findPalette.bind(this);
     this.savePalette = this.savePalette.bind(this);
   }
@@ -22,7 +23,13 @@ class App extends Component {
     });
   }
   savePalette(newPalette) {
-    this.setState({ palettes: [...this.state.palettes, newPalette] })
+    this.setState(
+      { palettes: [...this.state.palettes, newPalette] },
+      this.syncLocalStorage
+    );
+  }
+  syncLocalStorage() {
+    window.localStorage.setItem("palettes", JSON.stringify(this.state.palettes));
   }
   render() {
     return (
