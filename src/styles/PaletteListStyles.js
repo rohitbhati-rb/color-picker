aef3a0796b951f634715cc1571b33ba298275f52
Rohit Bhati <rohitbhati8833@gmail.com>
2021-07-08 11:04:23 +0530

    made paletteList responsive

--- src/styles/PaletteListStyles.js
+++ src/styles/PaletteListStyles.js
@@ -1,3 +1,4 @@
+import sizes from './sizes';
 const styles = {
     root: {
         backgroundColor: "blue",
@@ -12,6 +13,12 @@ const styles = {
         alignItems: "flex-start",
         flexDirection: "column",
         flexWrap: "wrap",
+        [sizes.down('lg')]: {
+            width: "80%"
+        },
+        [sizes.down('xs')]: {
+            width: "75%"
+        }
     },
     nav: {
         display: "flex",
@@ -19,7 +26,7 @@ const styles = {
         justifyContent: "space-between",
         alignItems: "center",
         color: "white",
-        "& a":{
+        "& a": {
             color: "white"
         }
     },
@@ -28,7 +35,14 @@ const styles = {
         width: "100%",
         display: "grid",
         gridTemplateColumns: "repeat(3, 30%)",
-        gridGap: "5%",
+        gridGap: "2.5rem",
+        [sizes.down('md')]: {
+            gridTemplateColumns: "repeat(2, 50%)"
+        },
+        [sizes.down('xs')]: {
+            gridTemplateColumns: "repeat(1, 100%)",
+            gridGap: "1rem"
+        }
     }
 };
 export default styles;
